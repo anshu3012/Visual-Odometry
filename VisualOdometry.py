2e6e624a093794c44a1e26b4f5d52f262cfea710
Kevin Dong <kevindong80@gmail.com>
2020-05-04 19:39:21 -0400

    Implemented findPointCorrespondences() in VisualOdometry class

--- VisualOdometry.py
+++ VisualOdometry.py
@@ -8,6 +8,7 @@ Copyright (c) 2020 Hao Da (Kevin) Dong, Anshuman Singh
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 '''
@@ -25,8 +26,50 @@ class VisualOdometry:
     @param      
     @return       
     '''
-    def findPointCorrespondences(self):
-        pass
+    def findPointCorrespondences(self, image1, image2):
+        # Initiate SIFT detector
+        orb = cv2.ORB_create()
+
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = orb.detectAndCompute(image1, None)
+        kp2, des2 = orb.detectAndCompute(image2, None)
+
+        FLANN_INDEX_LSH = 6
+        index_params= dict(algorithm = FLANN_INDEX_LSH,
+                           table_number = 6,        # 12
+                           key_size = 12,           # 20
+                           multi_probe_level = 1)   # 2
+        search_params = dict(checks = 50)
+        
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1, des2, k=2)
+
+        matchesMask = [[0,0] for i in range(len(matches))]
+
+        # Store all the good matches as per Lowe's ratio test.
+        good = []
+        for i, (m,n) in enumerate(matches):
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+                matchesMask[i]=[1,0]
+
+        pts1 = np.array([kp1[m.queryIdx].pt for m in good]).reshape(-1, 2)
+        pts2 = np.array([kp2[m.trainIdx].pt for m in good]).reshape(-1, 2)
+
+        '''
+        draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+        img3 = cv2.drawMatchesKnn(image1, kp1, image2, kp2, matches, None, **draw_params)
+
+        cv2.imshow('frame', cv2.resize(img3, (1280, 720)))
+        cv2.waitKey(0)
+        '''
+
+        return pts1, pts2
 
 
     '''
@@ -34,8 +77,26 @@ class VisualOdometry:
     @param      
     @return       
     '''
-    def getFundamentalMatrix(self):
-        pass
+    def getFundamentalMatrix(self, pts1, pts2):
+        numPoints = pts1.shape[0]
+        A = np.empty((numPoints, 9))
+
+        for i in range(numPoints): 
+            x1 = pts1[i,0]
+            y1 = pts1[i,1]
+            x2 = pts2[i,0]
+            y2 = pts2[i,1]
+            A[i] = np.array([x1*x2, x1*y2, x1, y1*x2, y2*y1, y1, x2, y2, 1])
+
+        U, S, VT = np.linalg.svd(A, full_matrices=True)     # Take SVD of A
+        f = VT[-1].reshape(3,3)                             # Last column of V matrix
+        
+        # Constrain Fundamental Matrix to Rank 2
+        u1,s1,v1 = np.linalg.svd(f) 
+        s2 = np.array([[s1[0], 0, 0], [0, s1[1], 0], [0, 0, 0]]) 
+        F = u1 @ s2 @ v1
+
+        return F
     
     
     '''
@@ -106,4 +167,18 @@ class VisualOdometry:
 
 
 if __name__ == '__main__':
-    pass
+    # Read test images
+    image1 = cv2.imread('test1.png', 0)       
+    image2 = cv2.imread('test2.png', 0) 
+
+    vOdom = VisualOdometry()
+
+    pts1, pts2 = vOdom.findPointCorrespondences(image1, image2)
+
+    F0 = vOdom.getFundamentalMatrix(pts1, pts2)
+    print(F0)
+
+    F,_ = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+
+    print(F)
+
